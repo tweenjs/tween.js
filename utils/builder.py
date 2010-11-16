967299c8bab4930e12d865c68070bf7e016fb53f
Mr.doob <info@mrdoob.com>
2010-11-16 13:00:50 +0000

    Now using Closure Compiler (~200bytes)

--- utils/builder.py
+++ utils/builder.py
@@ -3,7 +3,8 @@ import os
 
 output = '../build/Tween.js';
 
-os.system("java -jar yuicompressor-2.4.2.jar ../src/Tween.js -o ../build/Tween.js --charset utf-8 -v");
+# os.system("java -jar yuicompressor-2.4.2.jar ../src/Tween.js -o ../build/Tween.js --charset utf-8 -v");
+os.system("java -jar compiler.jar --js ../src/Tween.js --js_output_file ../build/Tween.js")
 
 # HEADER
 
