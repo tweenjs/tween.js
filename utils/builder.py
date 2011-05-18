aca3cf45ba32cdad69c232794497fc8033b63cc6
sole <listas@soledadpenades.com>
2011-05-18 18:16:17 +0100

    Update packer system to include REVISION number too

--- utils/builder.py
+++ utils/builder.py
@@ -1,14 +1,17 @@
 import sys
 import os
 
-output = '../build/Tween.js';
+output = '../build/tween.js';
 
 # os.system("java -jar yuicompressor-2.4.2.jar ../src/Tween.js -o ../build/Tween.js --charset utf-8 -v");
-os.system("java -jar compiler.jar --js ../src/Tween.js --js_output_file ../build/Tween.js")
+os.system("java -jar compiler.jar --js ../src/Tween.js --js_output_file %s" % (output))
 
 # HEADER
 
-string = "// Tween.js - http://github.com/sole/tween.js\n"
+with open(os.path.join('..', 'REVISION'), 'r') as handle:
+	revision = handle.read().rstrip()
+
+string = "// tween.js r%s - http://github.com/sole/tween.js\n" % (revision)
 
 src_file = open(output,'r')
 string += src_file.read()
