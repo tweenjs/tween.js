9e26ed62efc34e14fc269a8ef967190edefa2f66
Mr.doob <info@mrdoob.com>
2012-09-01 14:28:16 +0200

    Fixed builder.

--- utils/builder.py
+++ utils/builder.py
@@ -6,4 +6,4 @@ output = '../build/tween.min.js'
 os.system( 'java -jar compiler/compiler.jar --language_in=ECMASCRIPT5 --js ' + source + ' --js_output_file ' + output )
 
 with open(output,'r') as f: text = f.read()
-with open(output,'w') as f: f.write(("// tween.min.js - http://github.com/sole/tween.js\n" % os.path.basename(output)) + text)
+with open(output,'w') as f: f.write("// tween.js - http://github.com/sole/tween.js\n" + text)
