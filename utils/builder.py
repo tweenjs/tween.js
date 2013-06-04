1c97a06c6c51f494a143cc815affddd4e0872902
sole <listas@soledadpenades.com>
2013-06-04 09:57:08 +0100

    Move node.js/npm build stuff into utils/, also output npm package into build/npm (WIP)

--- utils/builder.py
+++ utils/builder.py
@@ -3,19 +3,26 @@ import shutil
 
 source = '../src/Tween.js'
 output = '../build/tween.min.js'
-nodeOutput = '../nodejs/index.js'
+NPM_DIR = '../build/npm/'
 readme = '../README.md'
-nodeReadme = '../nodejs/README.md'
+nodeOutput = NPM_DIR + 'index.js'
+nodeReadme = NPM_DIR + 'README.md'
+srcJSON = './npm/package.json'
+dstJSON = NPM_DIR + 'package.json'
 
 os.system( 'java -jar compiler/compiler.jar --language_in=ECMASCRIPT5_STRICT --js ' + source + ' --js_output_file ' + output )
 
-# header
-
+# Add header with library name and url to the compressed output
 with open(output,'r') as f: text = f.read()
 with open(output,'w') as f: f.write('// tween.js - http://github.com/sole/tween.js\n' + text)
 
+# Build npm package for node.js
+if not os.path.exists(NPM_DIR):
+	os.makedirs(NPM_DIR)
+
 shutil.copyfile(source, nodeOutput)
 shutil.copyfile(readme, nodeReadme)
+shutil.copyfile(srcJSON, dstJSON)
 
 with open(nodeOutput,'r') as f: text = f.read()
 with open(nodeOutput,'w') as f: f.write('// tween.js - http://github.com/sole/tween.js\n' + text + '\nmodule.exports=TWEEN;')
