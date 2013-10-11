282f0f772382d5e12e6e0c2784d7a20506e0b98b
Jono Brandel <jonobrandel@google.com>
2013-10-11 14:11:14 -0700

    added preservation flag in TWEEN.update to be able to go backward in time

--- src/Tween.js
+++ src/Tween.js
@@ -61,7 +61,7 @@ var TWEEN = TWEEN || ( function () {
 
 		},
 
-		update: function ( time ) {
+		update: function ( time, preserve ) {
 
 			if ( _tweens.length === 0 ) return false;
 
@@ -75,7 +75,7 @@ var TWEEN = TWEEN || ( function () {
 
 					i ++;
 
-				} else {
+				} else if ( !preserve ) {
 
 					_tweens.splice( i, 1 );
 
