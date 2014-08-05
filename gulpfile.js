c7848913c4eb11fa2805db93371c3c0c93673947
Soledad Penades <listas@soledadpenades.com>
2014-08-04 22:24:49 -0700

    generating header comment using package.json metadata

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var htmlreplace = require('gulp-html-replace');
+var concat = require('gulp-concat');
+
+var metadata = require('./package');
+var header = '// ' + metadata.name + ' v.' + metadata.version + ' ' + metadata.homepage;
 
 gulp.task('lint', function() {
 	return gulp.src('src/Tween.js')
