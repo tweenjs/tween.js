22a8c68fa5ddf3492b32f36ff6cdd45f398aa35b
Soledad Penades <listas@soledadpenades.com>
2014-08-05 08:06:08 -0700

    also test minified version automatically

--- test/unit/nodeunitheadless.js
+++ test/unit/nodeunitheadless.js
@@ -1,7 +1,10 @@
-var TWEEN = require('../../src/Tween.js');
+// We will unit test both the original library and the minified version
+var TWEEN_uncompressed = require('../../src/Tween.js');
+var TWEEN_min = require('../../build/tween.min.js');
 var getTests = require('./tests');
 
 module.exports = {
-	'tween': getTests(TWEEN)
+	'tween': getTests(TWEEN_uncompressed),
+	'tween_min': getTests(TWEEN_min)
 };
 
