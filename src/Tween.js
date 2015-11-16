e19a4c929bace618cd4a8ab1f36357f97a2353ae
Remco Kranenburg <remco@burgsoft.nl>
2015-11-16 12:19:12 +0100

    Code style fix

--- src/Tween.js
+++ src/Tween.js
@@ -864,7 +864,7 @@ TWEEN.Interpolation = {
 		// Node.js
 		module.exports = TWEEN;
 
-	} else if(root !== undefined) {
+	} else if (root !== undefined) {
 
 		// Global variable
 		root.TWEEN = TWEEN;
