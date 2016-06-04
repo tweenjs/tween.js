4a98a90d870e449033f90c65949517358fd8e0bd
Michael Casebolt <mikebolt@gmail.com>
2016-06-04 13:42:43 -0700

    Simplify Elastic.Out easing.

--- src/Tween.js
+++ src/Tween.js
@@ -611,10 +611,6 @@ TWEEN.Easing = {
 
 		Out: function (k) {
 
-			var s;
-			var a = 0.1;
-			var p = 0.4;
-
 			if (k === 0) {
 				return 0;
 			}
@@ -623,14 +619,7 @@ TWEEN.Easing = {
 				return 1;
 			}
 
-			if (!a || a < 1) {
-				a = 1;
-				s = p / 4;
-			} else {
-				s = p * Math.asin(1 / a) / (2 * Math.PI);
-			}
-
-			return (a * Math.pow(2, - 10 * k) * Math.sin((k - s) * (2 * Math.PI) / p) + 1);
+			return Math.pow(2, -10 * k) * Math.sin((k - 0.1) * 5 * Math.PI) + 1;
 
 		},
 
