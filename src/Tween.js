6843309618a9cc1d2eb0069ff84b830124bb9353
Michael Casebolt <mikebolt@gmail.com>
2016-06-04 13:34:55 -0700

    Simplify Elastic.In easing.

--- src/Tween.js
+++ src/Tween.js
@@ -597,10 +597,6 @@ TWEEN.Easing = {
 
 		In: function (k) {
 
-			var s;
-			var a = 0.1;
-			var p = 0.4;
-
 			if (k === 0) {
 				return 0;
 			}
@@ -608,15 +604,8 @@ TWEEN.Easing = {
 			if (k === 1) {
 				return 1;
 			}
-
-			if (!a || a < 1) {
-				a = 1;
-				s = p / 4;
-			} else {
-				s = p * Math.asin(1 / a) / (2 * Math.PI);
-			}
-
-			return - (a * Math.pow(2, 10 * (k -= 1)) * Math.sin((k - s) * (2 * Math.PI) / p));
+      
+			return -Math.pow(2, 10 * (k - 1)) * Math.sin((k - 1.1) * 5 * Math.PI);
 
 		},
 
