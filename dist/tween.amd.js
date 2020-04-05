51ec60101350dc0f055d2a75276fdcac6944487f
Leonid Khramov <leonidtheking@gmail.com>
2020-04-05 14:44:15 -0500

    fixed issue with not stopping chained tweens when the head chain is not playing. Also works with cyclic chains.

--- dist/tween.amd.js
+++ dist/tween.amd.js
@@ -1,6 +1,6 @@
 define(function () { 'use strict';
 
-	var version = '18.4.2';
+	var version = '18.5.0';
 
 	/**
 	 * Tween.js - Licensed under the MIT license
@@ -150,6 +150,7 @@ define(function () { 'use strict';
 		this._onStopCallback = null;
 		this._group = group || TWEEN;
 		this._id = TWEEN.nextId();
+		this._isChainStopped = false;
 
 	};
 
@@ -193,6 +194,8 @@ define(function () { 'use strict';
 
 			this._onStartCallbackFired = false;
 
+			this._isChainStopped = false;
+
 			this._startTime = time !== undefined ? typeof time === 'string' ? TWEEN.now() + parseFloat(time) : time : TWEEN.now();
 			this._startTime += this._delayTime;
 
@@ -235,6 +238,11 @@ define(function () { 'use strict';
 
 		stop: function () {
 
+			if (!this._isChainStopped) {
+				this._isChainStopped = true;
+				this.stopChainedTweens();
+			}
+
 			if (!this._isPlaying) {
 				return this;
 			}
@@ -249,7 +257,6 @@ define(function () { 'use strict';
 				this._onStopCallback(this._object);
 			}
 
-			this.stopChainedTweens();
 			return this;
 
 		},
