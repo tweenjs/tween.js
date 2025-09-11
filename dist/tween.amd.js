b72b69b3aab0a47a643166fb8db586466c5a3a3c
alphabetX <alphabet_working_pc@inner.mail>
2025-09-11 11:46:33 +0800

    fix(yoyo): fix yoyo wrong -- 20250911

--- dist/tween.amd.js
+++ dist/tween.amd.js
@@ -769,7 +769,7 @@ define(['exports'], (function (exports) { 'use strict';
                 }
                 this._onEveryStartCallbackFired = true;
             }
-            var elapsedTime = time - this._startTime;
+            var elapsedTime = this._yoyo ? Math.min(time - this._startTime, this._duration) : time - this._startTime;
             var durationAndDelay = this._duration + ((_a = this._repeatDelayTime) !== null && _a !== void 0 ? _a : this._delayTime);
             var totalTime = this._duration + this._repeat * durationAndDelay;
             var calculateElapsedPortion = function () {
