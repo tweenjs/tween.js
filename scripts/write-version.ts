4f01506b236e10b317c420447ded090a18e0fdfb
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2026-08-31 15:20:08 +0500

    chore: migrate to Bun

---
+++ scripts/write-version.ts
@@ -0,0 +1,6 @@
+const {version} = await Bun.file(`${import.meta.dir}/../package.json`).json()
+
+await Bun.write(
+	`${import.meta.dir}/../src/Version.ts`,
+	[`const VERSION = '${version}'`, 'export default VERSION', ''].join('\n'),
+)
